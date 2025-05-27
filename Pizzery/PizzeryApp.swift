0287064047d426db661ef876d6960e6c8cd85ba7
Ivan Volkov <VANLEG@yandex.ru>
2025-05-27 22:57:39 +0300

    Initial Commit

---
+++ Pizzery/PizzeryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzeryApp.swift
+//  Pizzery
+//
+//  Created by Иван Волков on 27.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct PizzeryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
